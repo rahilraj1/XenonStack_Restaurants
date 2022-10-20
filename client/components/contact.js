1f2438d8b6dfa7d4af96bd7c31d6fd3b66bc7919
Rahil Raj <98189273+rahilraj1@users.noreply.github.com>
2022-10-20 07:51:06 +0530

    Create contact.js

---
+++ client/components/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact=()=>
+{
+    return(
+        <div>
+           <p>Welcome</p>
+           <h1>We are ready to provide you a car</h1>
+        </div>
+    )
+}
+
+export default Contact
